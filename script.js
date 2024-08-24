587b40b5847d62285378416067fce5f868644484
042Ali <ali20090609@gmail.com>
2024-08-24 18:03:57 +0400

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+const acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    for (j = 0; j < acc.length; j++) {
+      if (j !== i) acc[j].nextElementSibling.style.maxHeight = null;
+    }
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
